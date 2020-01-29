aa8f505b3e1f6b64f726f186f67cb07cc6bf0fed
yuniel <yuniel.work@gmail.com>
2020-01-29 04:18:15 -0500

    probado en PROD

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const InternetEtecsaLoginService = require('./internet-etecsa-login-service');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4500;
 const iconn = new InternetEtecsaLoginService();
 
 app.get('/toggle', (req, res) => {
-
   iconn.toggle()
     .then((result) => res.status(200).send(result))
     .catch((err) => res.status(400).send(String(err)));
