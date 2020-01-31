0cb739f5d995731b603906ae053a72a3bb256a20
yuniel <yuniel.work@gmail.com>
2020-01-31 16:43:02 -0500

    configuración refactorizada

--- etc/config.js
+++ etc/config.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
-const env = process.env.NODE_ENV;
-
-const dev = {
+module.exports = {
   port: parseInt(process.env.PORT, 10) || 3000,
   headless: process.env.HEADLESS || false,
   creds: {
@@ -10,19 +8,3 @@ const dev = {
     password: process.env.NAUTA_PASSWORD
   }
 };
-
-const prod = {
-  port: parseInt(process.env.PORT, 10) || 4500,
-  headless: process.env.HEADLESS || true,
-  creds: {
-    username: process.env.NAUTA_USER,
-    password: process.env.NAUTA_PASSWORD
-  }
-};
-
-const config = {
-  dev,
-  prod
-};
-
-module.exports = config[env];
